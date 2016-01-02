f37e91ec7ce1ae2a6193e3ca10f883cc15e0924b
JD Reddaway <jdreddaway@gmail.com>
2016-01-01 22:19:28 -0800

    Adds test elections code from pre-maven attempt.

---
+++ preferential-voting/src/main/java/org/prefvoting/spi/v1/elections/ElectionBean.java
@@ -0,0 +1,9 @@
+package org.prefvoting.spi.v1.elections;
+
+import com.google.api.server.spi.config.ApiResourceProperty;
+
+public class ElectionBean {
+
+	@ApiResourceProperty
+	String test;
+}
---
+++ preferential-voting/src/main/java/org/prefvoting/spi/v1/elections/Elections.java
@@ -0,0 +1,33 @@
+package org.prefvoting.spi.v1.elections;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiMethod.HttpMethod;
+
+@Api (
+		name = "pv",
+		title = "Preferential Voting",
+		canonicalName = "PreferentialVoting",
+		version = "v1",
+		description = "API for managing elections and voting."
+)
+public class Elections {
+
+	@ApiMethod (
+		name = "elections.get", //TODO try removing period
+		httpMethod = HttpMethod.GET
+	)
+	public ElectionBean get() {
+		ElectionBean ret =  new ElectionBean();
+		ret.test = "Hello?!?";
+		return ret;
+	}
+	
+	@ApiMethod (
+		name = "elections.post",
+		httpMethod = HttpMethod.POST
+	)
+	public ElectionBean post(ElectionBean input) {
+		return input;
+	}
+}
